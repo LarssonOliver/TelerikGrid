4133722ffae6e30d15b4b6cb94a007b0f3546ced
Oliver Larsson <olli.larsson@hotmail.com>
2021-07-27 10:08:40 +0200

    initial commit

---
+++ TelerikLocalizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Blazor.Services;
+
+namespace TelerikGrid
+{
+	public class TelerikLocalizer : ITelerikStringLocalizer
+	{
+		private static readonly Dictionary<string, ResourceSet> Lang = new();
+
+		public TelerikLocalizer()
+		{
+			ResourceManager res = new("TelerikGrid.Resources.Telerik", typeof(TelerikLocalizer).Assembly); 
+			Lang.Add("en", Telerik.Blazor.Resources.Messages.ResourceManager.GetResourceSet(new CultureInfo("en"), true, true));
+			Lang.Add("sv", res.GetResourceSet(new CultureInfo("sv"), true, false));
+		}
+
+		public String this[String name] { get => Lang[CultureInfo.CurrentCulture.TwoLetterISOLanguageName].GetString(name); }
+	}
+}
