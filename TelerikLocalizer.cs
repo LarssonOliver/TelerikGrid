36eebdc419b56ebd063fc45aad9dd1853d70898b
Oliver Larsson <olli.larsson@hotmail.com>
2021-07-28 08:57:27 +0200

    Switched to Core 3.1

--- TelerikLocalizer.cs
+++ TelerikLocalizer.cs
@@ -11,11 +11,11 @@ namespace TelerikGrid
 {
 	public class TelerikLocalizer : ITelerikStringLocalizer
 	{
-		private static readonly Dictionary<string, ResourceSet> Lang = new();
+		private static readonly Dictionary<string, ResourceSet> Lang = new Dictionary<string, ResourceSet>();
 
 		public TelerikLocalizer()
 		{
-			ResourceManager res = new("TelerikGrid.Resources.Telerik", typeof(TelerikLocalizer).Assembly); 
+			ResourceManager res = new ResourceManager("TelerikGrid.Resources.Telerik", typeof(TelerikLocalizer).Assembly); 
 			Lang.Add("en", Telerik.Blazor.Resources.Messages.ResourceManager.GetResourceSet(new CultureInfo("en"), true, true));
 			Lang.Add("sv", res.GetResourceSet(new CultureInfo("sv"), true, false));
 		}
